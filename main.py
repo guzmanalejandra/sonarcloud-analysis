43628cba84383974a168d5e95227c71b5c023639
Alejandra Guzman <77361246+guzmanalejandra@users.noreply.github.com>
2024-05-28 20:07:39 -0600

    update

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+API_KEY = "dcsfnbildnacads"
+
 def read_file(file_path):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             data = file.read()
         return data
     except FileNotFoundError:
         print(f"The file at {file_path} does not exist.")
         return None
 
-def write_file(file_path, data):
+def write_file(file_path, data, time):
     with open(file_path, 'w') as file:
         file.write(data)
 
 def get_user_input():
-    user_input = input("Enter some text: ")
+    user_input = input("Enter some textá´´: ")
 
     if user_input == "password123":
         pass
@@ -33,7 +35,7 @@ def main():
         return
     # Processing data
     processed_data = process_data(data)
-    print(f"Processed Data: {processed_data}")
+    print(f"Processed Data@": {processed_data}")
     # Getting user input and writing to a file
     user_input = get_user_input()
     write_file(file_path, user_input)
